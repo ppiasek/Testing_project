d3283ccc80c787e1401f2fcd6ed452349a5b9843
xantek-code <xantispl@gmail.com>
2019-09-15 19:43:33 +0200

    UI tests updates.

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 # Testing_project
 
-To connect to specific Gdrive account, credentials.json file should be downloaded from google developer console.
+# Basic configuration info
+To connect to specific Gdrive account via API, json file should be downloaded from google developer
+console. Location of that file should be provided in api.py _json_credentials variable. Also _token variable should
+be filled with location of token.pickle file or if it does not exist yet, the location where we want to save it.
 
-Selenium drivers need to be provided in test_base.py module with use of forward slashes.
+Location of Selenium drivers need to be provided in test_base.py module with use of forward slashes.
 driver_location variable contains folder location of all required drivers
 browser_driver variable contains names/remaining location of all required drivers
+tested_url variable stores url for our site ti test.
 
 # What works
-- Login with Selenium in Chrome browser (Windows / Linux?)
+- Login with Selenium in Chrome browser [Windows / Linux?]
+- Going through left side menu via Selenium
+- Logout with Selenium in Chrome browser (Only by XPath for user icon)
 - API login
 - API download file by id
 - API upload file
 
+# What semi works
+- Assertions that verify if correct menu is selected does not work for My Drive and Recent options
+- Assertions that verify if correct menu is selected work only with XPath
+
 # Known bugs
-- Login with Selenium does not work in Firefox browser, due to problem with clicking "Next" button after providing
-password (Windows / Linux?)
+- Selenium tests on Firefox browser are not working due to inability to log into google account [Windows / Linux?]
+- For some reason, selecting user icon cannot be done by class name. XPath works well.
--- gdrive_api_tests/selenium_test.py
+++ gdrive_api_tests/selenium_test.py
@@ -1,5 +1,5 @@
 import unittest
-from handlers.login_page import LoginPage
+from handlers.gdrive_page import GDrivePage
 from pathlib import Path
 
 
@@ -7,7 +7,7 @@ class UITest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = LoginPage('chrome')
+        cls.driver = GDrivePage('chrome')
         _credentials = Path('../credentials/login_credentials.txt').read_text()
         cls.username = _credentials[:_credentials.find('\n')]
         cls.password = _credentials[_credentials.find('\n') + 1:]
@@ -17,15 +17,29 @@ class UITest(unittest.TestCase):
         self.driver.login_field.clear()
         self.driver.login_field.send_keys(self.username)
         self.driver.login_field_next_button.click()
-        self.driver.wait_implictly(5)
         self.driver.password_field.clear()
         self.driver.password_field.send_keys(self.password)
-        self.driver.wait_implictly(5)
-        #self.driver.password_field_next_button.click()
-        self.driver._driver.execute_script('arguments[0].click();', self.driver._driver.find_element_by_id('passwordNext'))
-        #self.assertTrue(self.driver.user_logged_in, "User is not logged in")
+        self.driver.password_field_next_button.click()
+        #self.driver._driver.execute_script('arguments[0].click();', self.driver._driver.find_element_by_id('passwordNext'))
+        self.assertTrue(self.driver.user_logged_in, "User is not logged in")
+
+    def test_2_go_through_tree(self):
+        self.driver.shared_with_me_button.click()
+        self.assertTrue(self.driver.shared_with_me_text.is_displayed(), 'Shared with me not displayed')
+        self.driver.recent_button.click()
+        #self.assertTrue(self.driver.recent_text.is_displayed(), 'Recent not displayed')
+        self.driver.starred_button.click()
+        self.assertTrue(self.driver.starred_text.is_displayed(), 'Starred not displayed')
+        self.driver.trash_button.click()
+        self.assertTrue(self.driver.trash_text.is_displayed(), 'Trash not displayed')
+        self.driver.my_drive_button.click()
+        #self.assertTrue(self.driver.my_drive_text.is_displayed(), 'My Drive not displayed')
+
+    def test_3_logout(self):
+        self.driver.user_view.click()
+        self.driver.user_logout.click()
+        self.assertTrue(self.driver.user_logged_out.is_displayed(), "User not logged out")
 
     @classmethod
     def tearDownClass(cls):
-        #cls.driver.close()
-        pass
+        cls.driver.close()
